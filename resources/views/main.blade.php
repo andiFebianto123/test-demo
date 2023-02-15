8a5166700c43085e6d7b9910feda1bdafcfc2801
andiFebianto123 <andifebianto123@gmail.com>
2023-02-15 16:30:04 +0700

    test demo program mahasiswa

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Data Mahasiswa</title>
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="{{ asset('DataTables/datatables.min.css') }}"/>
     <script src="{{ asset('js/jquery.min.js') }}"></script>
     <style>
         .dataTables_length {
@@ -22,7 +21,6 @@
         </div>
     </main>
     @stack('scripts')
-    <script type="text/javascript" src="{{ asset('DataTables/datatables.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
